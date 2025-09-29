2cc1a4bed605909b754a459cbcede282eead910e
Galym <forjob1351@gmail.com>
2025-09-29 10:29:07 +0500

    practice: MRO example

--- 05.Inheritance/inheritance practice session.py
+++ 05.Inheritance/inheritance practice session.py
@@ -1,17 +1,26 @@
-class Date(object):
-    def get_date(self):
-        return "2016-05-14"
+# class Date(object):
+#     def get_date(self):
+#         return "2016-05-14"
 
 
-class Time(Date):
-    def get_time(self):
-        return "07:00:00"
+# class Time(Date):
+#     def get_time(self):
+#         return "07:00:00"
 
 
-dt = Date()
-tm = Time()
+# dt = Date()
+# tm = Time()
 
-print('просто дата',dt.get_date())
-print('просто время',tm.get_time())
-print('просто дата и время через класс Time',tm.get_date() + ' ' + tm.get_time())
+# print('просто дата',dt.get_date())
+# print('просто время',tm.get_time())
+# print('просто дата и время через класс Time',tm.get_date() + ' ' + tm.get_time())
 
+class A: pass
+class B(A): pass
+class C(A): pass
+
+class D(B, C): pass
+class E(C, B): pass
+
+
+print(E.mro())
